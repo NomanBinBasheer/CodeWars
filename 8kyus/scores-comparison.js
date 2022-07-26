8ee3b61e21476b098615c23231219dde11f8da61
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-07-26 20:43:11 +0500

    Added a new 8kyu codewars solution

---
+++ 8kyus/scores-comparison.js
@@ -0,0 +1,28 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+
+// MY SOLUTION
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    let pointsOfClass = classPoints.reduce((acc, c) => acc + c)
+    let numOfStu = classPoints.length
+    
+    let classScores = pointsOfClass/numOfStu
+    
+    if ( yourPoints > classScores){
+      return true
+    }
+    else{
+      return false
+    }
+  }
