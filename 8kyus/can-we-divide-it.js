1170b94d0367e7bf9d5532e54e20d28aefcca739
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-07-27 10:20:10 +0500

    added a new 8kyu CodeWars solution

---
+++ 8kyus/can-we-divide-it.js
@@ -0,0 +1,34 @@
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+// A few cases:
+
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+
+// (4, 1, 4)     ->  true
+// (15, -5, 3)   ->  true
+
+
+// My Solution
+
+function isDivideBy(number, a, b) {
+    if (number % a === 0 && number % b === 0){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+
+  
+//   My 2nd and shorter Solution
+
+function isDivideBy(number, a, b) {
+    return number % a === 0 && number % b === 0
+    
+  }
