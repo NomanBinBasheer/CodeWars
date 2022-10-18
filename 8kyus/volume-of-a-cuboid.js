3d73047bde0838170314378c857099331a791e63
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-10-18 22:48:23 +0500

    added a new 8kyu solution

---
+++ 8kyus/volume-of-a-cuboid.js
@@ -0,0 +1,11 @@
+/*Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.*/
+
+
+// My Solution 
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      let colOfCuboid = length * width * height
+      return colOfCuboid
+    }
+  }
