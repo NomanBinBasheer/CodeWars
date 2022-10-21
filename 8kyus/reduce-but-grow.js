0a8d168196b8b4a98f6a80992efe211fe1caa245
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-10-21 18:43:03 +0500

    added a new 8kyu solution

---
+++ 8kyus/reduce-but-grow.js
@@ -0,0 +1,10 @@
+/* Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 */
+
+
+//  My Solution
+
+function grow(x){
+    return x.reduce((a,b) => a*b)
+  }
