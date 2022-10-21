4c36073e6955f0d6cbe68265bfaedf28a7c25eea
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-10-21 20:57:12 +0500

    added anew 8kyu soution

---
+++ 8kyus/convert-string-to-array.js
@@ -0,0 +1,14 @@
+/* Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"] */
+
+
+//  My Solution
+
+function stringToArray(string){
+    let strToArr = string.split(' ')
+     return strToArr
+   }
