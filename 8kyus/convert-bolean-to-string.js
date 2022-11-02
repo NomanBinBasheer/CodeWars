b84f68bc72bebb1c487d9ed4486326c5bffdc44c
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-11-02 23:12:00 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/convert-bolean-to-string.js
@@ -0,0 +1,11 @@
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. */
+
+
+
+//  My Solution
+
+function booleanToString(b){
+    return b.toString()
+  }
