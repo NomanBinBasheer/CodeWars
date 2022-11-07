8b5356389dfb8e0b4bbe3ec5593bd6d8efde6fda
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-11-07 23:25:43 +0500

    added a new 8kyu solution

---
+++ 8kyus/how-many-lightsbars.js
@@ -0,0 +1,21 @@
+/* The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+Note: your function should have a default parameter.
+
+For example(Input --> Output):
+
+"anyone else" --> 0
+"Zach" --> 18 */
+
+
+
+//  My Solution
+
+function howManyLightsabersDoYouOwn(name) {
+    if (name === "Zach"){
+      return 18
+    }
+     else{
+       return 0
+     }
+   }
