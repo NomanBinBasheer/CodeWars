dd890336b3c8859734adf17fbc594c6041987fd0
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-11-09 13:32:42 +0500

    added a new 7kyu codewars solution

---
+++ 7kyus/sum-of-angles.js
@@ -0,0 +1,9 @@
+/*Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2. */
+
+
+// My Solution
+
+function angle(n) {
+    let sum = (n-2) * 180
+    return sum
+  }
