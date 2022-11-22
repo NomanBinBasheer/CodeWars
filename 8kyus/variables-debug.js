1d0fde9b6750bc82c6cd52dc9d808c9425d12ade
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-11-22 22:04:14 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/variables-debug.js
@@ -0,0 +1,11 @@
+/*Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name. */
+
+
+
+//  My Solution
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
