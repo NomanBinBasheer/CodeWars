c9c5c69396f0eb875c5402b5d1dc357c4e842c85
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-12-01 22:08:54 +0500

    added a new 8kyu codewars solution

--- 8kyus/switch.js
+++ 8kyus/switch.js
@@ -92,3 +92,14 @@ function howManydays(month){
     }
     return days;
   }
+
+
+  //  Also 
+
+  function howManydays(month){
+    switch ( month ){
+      case 2 : return 28
+      case 4: case 6: case 9: case 11: return 30
+      default: return 31
+    }
+  }
