3768b0377384d5e0b228cf9ffb5aa002aa90f1cb
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-12-06 22:31:26 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/ensure-question.js
@@ -0,0 +1,19 @@
+/* Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?" */
+
+
+
+//  My Solution
+
+function ensureQuestion(s) {
+    if (s.endsWith('?')){
+        return s
+    }
+    else{
+      return `${s}?`
+    }
+  }
