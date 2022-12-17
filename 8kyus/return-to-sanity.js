6238814d02046cafe7964dfbae1f8c1d7dd37f36
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-12-17 18:37:37 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/return-to-sanity.js
@@ -0,0 +1,11 @@
+/* This function should return an object, but it's not doing what's intended. What's wrong? */
+
+
+
+//  My Solution
+
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
