02fa871d51c07d3d1e11b5b7140a6f3472ae1541
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-12-30 12:03:07 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/fix-my-first-kata.js
@@ -0,0 +1,21 @@
+/* function myFirstKata(a, b) {
+    if (typeof(a) == "number" && typeof(b) == "number") {
+      return a % b + b % a;
+    }
+    else {
+      return false
+    }
+  } */
+
+
+
+//  My Solution
+
+function myFirstKata(a, b) {
+    if (typeof(a) == "number" && typeof(b) == "number") {
+      return a % b + b % a;
+    }
+    else {
+      return false
+    }
+  }
