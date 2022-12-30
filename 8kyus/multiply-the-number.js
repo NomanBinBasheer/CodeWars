4efcf519cc30092f402c25d46b33f8cefe307ecb
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2022-12-30 11:14:25 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/multiply-the-number.js
@@ -0,0 +1,21 @@
+/*Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+multiply(3)==15 // 3 * 5¹
+multiply(10)==250 // 10 * 5²
+multiply(200)==25000 // 200 * 5³
+multiply(0)==0 // 0 * 5¹
+multiply(-3)==-15 // -3 * 5¹ */
+
+
+
+//  My Solution
+
+function multiply(number){
+  let str = number.toString()
+  if (number >= 0){
+  return str * Math.pow(5, str.length)
+  }
+  else{
+    return str * Math.pow(5, str.length - 1)
+  }
+}
