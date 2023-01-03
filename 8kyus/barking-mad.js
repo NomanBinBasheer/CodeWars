7d23cd9ef6a8f747433d97cd3ff471278d0386f0
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2023-01-03 18:49:34 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/barking-mad.js
@@ -0,0 +1,27 @@
+/* Teach snooTeach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefinedpy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined */
+
+
+//  My Solution
+
+function Dog (breed) {
+    this.breed = breed;
+  }
+  
+  var snoopy = new Dog("Beagle");
+  
+  snoopy.bark = function() {
+    return "Woof";
+  };
+  
+  var scoobydoo = new Dog("Great Dane");
+  
+  scoobydoo.bark = function() {
+    return "Woof";
+  };
+  
