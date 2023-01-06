29811e051f6a2b9cdefd7830204418d205cc1434
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2023-01-06 23:47:36 +0500

    added a new 8kyu codewars solution

---
+++ 8kyus/mr-freeze.js
@@ -0,0 +1,9 @@
+/* There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it. */
+
+
+
+//  My Solution
+
+// mark the MrFreeze object instance as frozen
+
+Object.freeze(MrFreeze)
