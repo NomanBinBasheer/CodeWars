5a0a838e0096faaa78686ddef0f660db36782e2a
Noman Bin Basheer <nomanbinbasheer66626@gmail.com>
2023-02-08 13:20:04 +0500

    added a new 7kyu codewars solution

---
+++ 7kyus/find-the-vowels.js
@@ -0,0 +1,28 @@
+/* We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!) */
+
+
+
+//  My Solution
+
+function vowelIndices(word){
+    let vowelsIndexes = []
+    let splitted = word.split('')
+    for (let i = 0; i < splitted.length; i++){
+      let low = splitted[i].toLowerCase()
+      if(low == 'a' || low == 'e' || low == 'i' || low == 'o' || low == 'u' || low == 'y'){
+        vowelsIndexes.push(i+1)
+      }
+    }
+    return vowelsIndexes
+  }
